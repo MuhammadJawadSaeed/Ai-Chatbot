171467c647889f324f6a22eb75acbec5cc8b4400
MuhammadJawadSaeed <jawadsaeed917@gmail.com>
2025-09-17 00:22:15 +0500

    socket io and ai integration

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const app = require("./src/app");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/service/ai.service");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+  socket.on("ai-message", async (data) => {
+    const response = await generateResponse(data.prompt);
+    console.log("ai response: ", response);
+    socket.emit("ai-message-response", { response });
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is runing or port 3000");
+});
